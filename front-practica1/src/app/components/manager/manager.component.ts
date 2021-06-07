27dda3a7ce115dfdd17eb59ce7768a6074ccf823
José Rafael Morente González <josemorenteg98@outlook.com>
2021-06-06 23:38:31 -0600

    [UPDATED] manager.component.*, app.module.ts, index.html, nav-bar.component.html Actualizacion de agregado de productos.

--- front-practica1/src/app/components/manager/manager.component.ts
+++ front-practica1/src/app/components/manager/manager.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {MatTable} from '@angular/material/table';
+import { ProductosService } from 'src/app/services/producto.service';
+import { UploadFileService } from 'src/app/services/upload-file.service';
+
+//SWAL
+declare var swal:any;
+//JQUERY
+declare var $:any;
 
 @Component({
   selector: 'app-manager',
@@ -7,8 +15,28 @@ import {MatTable} from '@angular/material/table';
   styleUrls: ['./manager.component.scss']
 })
 export class ManagerComponent implements OnInit {
+	/**
+	 * PROPIEDADES FORMULARIO
+	 */
+	formData:FormGroup = new FormGroup({
+		'id': new FormControl(0),
+		'nombre': new FormControl("", [Validators.required, Validators.maxLength(100)]),
+		'descripcion': new FormControl("", [Validators.required, Validators.maxLength(500)]),
+		'precio': new FormControl(0, [Validators.required]),
+		'costo': new FormControl(0, [Validators.required]),
+		'cantidad': new FormControl(1, [Validators.required]),
+		'imagen': new FormControl("https://upload.wikimedia.org/wikipedia/commons/4/4a/Usac_logo.png", [Validators.required]),
+	});
+	/**
+	 * OTRAS PROPIEDADES
+	 */
+	public strImage: any = "";
+	public file: File;
 
-	constructor() { }
+	constructor(
+		private productoService: ProductosService,
+    	private uploadFileService: UploadFileService,
+	) { }
 
 	// Aqui van los nombres de las columnas 
 	columnas: string[] = ['Ejemplo'];
@@ -25,10 +53,31 @@ export class ManagerComponent implements OnInit {
 	ngOnInit(): void {
 	}
 	
-	// Método Agregar
+	
+	/**
+	 * METODO PARA AGREGAR PRODUCTO
+	 */
 	agregar(): void {
+		console.log(this.formData.value)
+		this.productoService.create(this.formData.value)
+		.subscribe((res) => {
+		console.log(res)
+		$('#modalFormDataAdd').modal('hide');
+		swal({
+			title: "Producto Agregado",
+			text: "El producto se ha agregado exitosamente.",
+			type: "success",
+			icon: "success",
+		});
 		
-		
+		}, (error) => {
+		swal({
+			title: "Error",
+			text: "Ha ocurrido un error. Intentélo más tarde.",
+			type: "error",
+			icon: "error",
+		});
+		});
 	}
 	
 	// Método Modificar Este Id es basicamente la posicion que tiene en la tabla de la vista pero si empezamos el id de la base desde 1 
@@ -45,5 +94,63 @@ export class ManagerComponent implements OnInit {
 		
 		
 	}
+
+	/**
+	 * SELECCION DE IMAGEN
+	 * @param archivo
+	 */
+	seleccionImage( archivo: File | null ): void {
+		if ( !archivo ) {
+		  this.file = null;
+		  return;
+		}
+	
+		if ( archivo.type.indexOf('image') < 0 ) {
+		  swal({
+			title: "Seleccionar imagen",
+			text: "Debe de seleccionar una imagen.",
+			icon: "info",
+		  });
+		  this.file = null;
+		  return;
+		}
+	
+		this.file = archivo;
+		let reader = new FileReader();
+		let urlImagenTemp = reader.readAsDataURL( archivo );
+		reader.onloadend = () => this.strImage = reader.result;
+	}
+
+	/**
+	 * METODO PARA SUBIR IMAGEN A CLOUDINARY
+	 */
+	uploadImage(): void {
+		this.uploadFileService.subirArchivo( this.file, 'AYDS1/Producto' )
+		.then((res:any) => {
+			console.log(res);
+		  this.strImage = res.secure_url;
+		  this.imagen?.setValue(this.strImage);
+		  swal({
+			title: "Foto cargada",
+			text: "La imagen se ha subido exitosamente.",
+			icon: "success",
+		  });
+		})
+		.catch((resp) => {
+		  swal({
+			title: "Error",
+			text: "Ha ocurrido un error. Intente mas tarde nuevamente.",
+			icon: "error",
+		  });
+		});
+	}
+
+	get id() { return this.formData.get('id'); }
+	get nombre() { return this.formData.get( 'nombre'); }
+	get descripcion() { return this.formData.get('descripcion'); }
+	get precio() { return this.formData.get('precio'); }
+	get costo() { return this.formData.get('costo'); }
+	get cantidad() { return this.formData.get('cantidad'); }
+	get imagen() { return this.formData.get('imagen'); }
 
 }
---
+++ front-practica1/src/app/services/producto.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError} from 'rxjs/operators';
+import { path } from '../config.module';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  private basePath:string = path.path;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+  
+  constructor(private http: HttpClient) { }
+
+  //HANDLE ERROR
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Un error ha ocurrido:', error.error.message);
+    } else {
+      console.error(
+      `Backend returned code ${error.status}, ` +
+      `body was: `, error.error);
+    }
+    return throwError(
+      error);
+  };
+
+  //CREATE
+  public create(data:any) : Observable<any> {
+    let url = `${this.basePath}producto`;
+    return this.http.post(url, data, this.httpOptions)
+    .pipe(
+      catchError(this.handleError)
+    )
+  }
+
+
+}
